a749879a7eba8acc9393256ccdcd302635c6d617
OksanaP <77opod@gmail.com>
2022-08-23 13:16:11 +0300

    first commit

---
+++ 58/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.WriteLine($"\nВведите размеры матриц.");
+int m = InputNumbers("Введите число строк матриц: ");
+int n = InputNumbers("Введите число столбцов матриц: ");
+int[,] firstMartrix = new int[m, n];
+CreateArray(firstMartrix);
+Console.WriteLine($"\nПервая матрица:");
+WriteArray(firstMartrix);
+int[,] secomdMartrix = new int[m, n];
+CreateArray(secomdMartrix);
+Console.WriteLine($"\nВторая матрица:");
+WriteArray(secomdMartrix);
+int[,] resultMatrix = new int[m, n];
+MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+Console.WriteLine($"\nПроизведение первой и второй матриц:");
+WriteArray(resultMatrix);
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+for (int i = 0; i < resultMatrix.GetLength(0); i++)
+{
+for (int j = 0; j < resultMatrix.GetLength(1); j++)
+{
+int sum = 0;
+sum = firstMartrix[i,j] * secomdMartrix[i,j];
+resultMatrix[i,j] = sum;
+}}}
+int InputNumbers(string input)
+{
+Console.Write(input);
+int output = Convert.ToInt32(Console.ReadLine());
+return output;
+}
+void CreateArray(int[,] array)
+{
+for (int i = 0; i < array.GetLength(0); i++)
+{
+for (int j = 0; j < array.GetLength(1); j++)
+{
+array[i, j] = new Random().Next(1,10);
+}}}
+void WriteArray (int[,] array)
+{
+for (int i = 0; i < array.GetLength(0); i++)
+{
+for (int j = 0; j < array.GetLength(1); j++)
+{
+Console.Write(array[i,j] + " ");
+}
+Console.WriteLine();
+}}
---
+++ 62/Program.cs
@@ -0,0 +1,31 @@
+﻿int n = 4;
+int[,] sMatrix = new int[n, n];
+int temp = 1;
+int i = 0;
+int j = 0;
+while (temp <= sMatrix.GetLength(0) * sMatrix.GetLength(1))
+{
+sMatrix[i, j] = temp;
+temp++;
+if (i <= j + 1 && i + j < sMatrix.GetLength(1) - 1)
+j++;
+else if (i < j && i + j >= sMatrix.GetLength(0) - 1)
+i++;
+else if (i >= j && i + j > sMatrix.GetLength(1) - 1)
+j--;
+else
+i--;
+}
+WriteArray(sMatrix);
+void WriteArray (int[,] array)
+{
+for (int i = 0; i < array.GetLength(0); i++)
+{
+for (int j = 0; j < array.GetLength(1); j++)
+{
+if (array[i,j] / 10 <= 0)
+Console.Write($" {array[i,j]} ");
+else Console.Write($"{array[i,j]} ");
+}
+Console.WriteLine();
+}}
